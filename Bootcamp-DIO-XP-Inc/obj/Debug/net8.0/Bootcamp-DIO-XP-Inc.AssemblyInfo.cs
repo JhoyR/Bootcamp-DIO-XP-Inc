e83f1b5b74fcd123d7ad2b0038f247f16b8c77e4
Jhoy Rodrigues <jhoymartins@gmail.com>
2024-09-12 08:48:20 -0300

    feat: Introdução Banco de Dados

--- Bootcamp-DIO-XP-Inc/obj/Debug/net8.0/Bootcamp-DIO-XP-Inc.AssemblyInfo.cs
+++ Bootcamp-DIO-XP-Inc/obj/Debug/net8.0/Bootcamp-DIO-XP-Inc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bootcamp-DIO-XP-Inc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e76cbfe744d03b3598ed787d0c215ffe22edbfca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fac4cedbade180578674c791c8e44a6944ba88")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bootcamp-DIO-XP-Inc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bootcamp-DIO-XP-Inc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
